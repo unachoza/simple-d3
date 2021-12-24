0686164b460e482ee29832ca9e0eec1311e4c487
Arianna Choza <unachoza@gmail.com>
2021-12-24 17:26:10 -0500

    d3 set up useRef grabbing film permits from nyc data

--- src/App.js
+++ src/App.js
@@ -5,18 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Arianna Choza Learns D3 Finally<br></br>
+        She really needs to learn testing next
       </header>
     </div>
   );
